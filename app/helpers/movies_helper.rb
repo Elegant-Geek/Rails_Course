75a1ba3f1575c4bb2a5b8ea2b711895e39e25cc1
Jamie Clark <clarkcjamie@gmail.com>
2022-02-25 22:58:54 -0500

    Fifth commit, done with module 7

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,17 @@
 module MoviesHelper
+    def total_gross(movie)
+        if movie.flop?
+           "Flop" 
+        #    ^^ NO "PUTS" IS NEEDED
+        else
+            number_to_currency(movie.total_gross, precision: 0)
+        end
+    end
+
+    def year_of(movie)
+        # movie.released_on.strftime("%Y")
+        # not efficient ^^^
+        movie.released_on.year
+        # shortcut! ^^^
+    end
 end
