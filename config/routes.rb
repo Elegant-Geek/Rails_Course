a57b1e5f5acab44d64ee47ebaa3fee1bb17ac65a
Jamie Clark <clarkcjamie@gmail.com>
2022-02-27 17:26:47 -0500

    Seventh commit, done with module 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "movies" => "movies#index"
+  get "movies/:id" => "movies#show"
   #The syntax for adding a route is a bit unorthodox, so here's a reminder of the generic format:
   #verb "url" => "name_of_controller#name_of_action"
 
