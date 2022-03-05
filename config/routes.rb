4433be6b3be92bc5de833959b7f0ad0eaf50fcc3
Jamie Clark <clarkcjamie@gmail.com>
2022-03-04 23:55:59 -0500

    11th commit, done with Module 13

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
   root "movies#index"
+  resources :movies
 
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  patch "movies/:id" => "movies#update"
+
+  # get "movies" => "movies#index"
+  # get "movies/:id" => "movies#show", as: "movie"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # patch "movies/:id" => "movies#update"
+
   #The syntax for adding a route is a bit unorthodox, so here's a reminder of the generic format:
   #verb "url" => "name_of_controller#name_of_action"
 
