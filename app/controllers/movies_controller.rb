c14d6088b367fc35c4c6227fe9ccb63c06e77e05
Jamie Clark <clarkcjamie@gmail.com>
2022-03-06 22:54:50 -0500

    15th commit, done with Module 17

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,5 +39,6 @@ end
 
 private
     def movie_params
-        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross,
+                                :director, :duration, :image_file_name)
     end
