c1fe9b1d468394f6f09a12e518100bdc9f3c0896
Jamie Clark <clarkcjamie@gmail.com>
2022-03-06 20:23:47 -0500

    14th commit, done with Module 16

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
 
     def index #action name goes here, index in this case. (which is tied to the name.html.erb view template file)
         # @movies = ["Iron Man", "Superman", "Spider-Man", "Avengers"]
-        @movies = Movie.all
+        @movies = Movie.released
     end
 
     def show
