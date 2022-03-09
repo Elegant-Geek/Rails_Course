be7e8b5d72361c38c486d4519ee3ca49ed8c6e73
Jamie Clark <clarkcjamie@gmail.com>
2022-03-09 02:31:22 -0500

    21st commit, done with Module 23

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,9 @@ class Review < ApplicationRecord
   validates :comment, length: { minimum: 4 }
   STARS = [1, 2, 3, 4, 5]
   validates :stars, inclusion: { in: STARS, message: "stars must be between 1 and 5" }
+
+  def stars_as_percent
+    (stars / 5.0) * 100.0
+  end
+
 end
