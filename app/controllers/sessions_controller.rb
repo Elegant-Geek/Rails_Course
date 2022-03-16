3b6d437086c1a0df02bcf04cff5ada50835dc200
Jamie Clark <clarkcjamie@gmail.com>
2022-03-16 00:02:05 -0400

    28th ci, finished module 30, authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
     end
 
     def create
-        fail
+    user = User.find_by(username: params[:username_or_email]) ||
+    user = User.find_by(email: params[:username_or_email]) 
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+        flash.now[:alert] = "Invalid email/password combination!"
+        render :new
+    end
     end
 
     def destroy
