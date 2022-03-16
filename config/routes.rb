cbc834cc39ea5abd7be1f1b8feb0029fa3ca2a90
Jamie Clark <clarkcjamie@gmail.com>
2022-03-15 21:40:45 -0400

    27th ci, finished module 29, new session form

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews
   end
+  resource :session, only: [:new, :create, :destroy]
   get "signup" => "users#new"
+  get "login" => "sessions#new"
 
 
   # get "movies" => "movies#index"
