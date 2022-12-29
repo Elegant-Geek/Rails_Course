b6c66e65ee8a9d11a795ba52d47d4992cd109203
Jamie Clark <clarkcjamie@gmail.com>
2022-12-28 21:16:38 -0500

    change ruby version to 3.1.1

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Flix
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # #fix forbidden class error in heroku yaml
+    # config.active_record.yaml_column_permitted_classes = [Symbol, Date, Time]
+  config.active_record.use_yaml_unsafe_load = true
   end
 end
